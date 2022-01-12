76baf8a8d5005c651612771a7549a6f00146788c
AndreFrigo <andrea.boy.frigo@gmail.com>
2022-01-12 18:15:28 +0100

    Create compareTrials function and continue with Pearson

--- main.py
+++ main.py
@@ -79,6 +79,38 @@ def normalizePatient(patient):
                trial[1] -= tot
     return ret
 
+#longest common subsequence between two lists
+def lcs_length(a, b):
+    table = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
+    for i, ca in enumerate(a, 1):
+        for j, cb in enumerate(b, 1):
+            table[i][j] = (
+                table[i - 1][j - 1] + 1 if ca == cb else
+                max(table[i][j - 1], table[i - 1][j]))
+    return table[-1][-1]
+
+#compare one sequence of trials (list trial) of one patient with all the sequences of trials for one specific condition of an other patient
+#based on LCS to keep the order information, normalized over the length of the longest element between the two trials of the patients
+#returns the success rate of the second patient for the most similar trial and the similarity value
+#makes the comparison only if the last therapy is the same for both, otherwise the value for that comparison is 0, this is because of how the different trials lists are made
+def compareTrials(trial1, condition2):
+    #trial1 is supposed to be a list containing only therapies (es. [th1, th2, th3], es2. [th1])
+    #condition2 is supposed to be a list of lists of therapies(list) and the last success rate
+        #(es. [ [[th1], 80], [[th1, th2, th3],100] ]
+    similarity=[0,0,0]
+    last = trial1[-1]
+    for elem in condition2:
+        if (elem[0][-1] == last):
+            #they have the same final therapy applied, so check the similarity (that will be non-zero)
+            sim = lcs_length(trial1, elem[0])
+            if(sim > similarity[0] and elem[1] != None):
+                #if it is more similar then the others and the success rate is defined, store both the values
+                similarity[0] = sim
+                similarity[1] = elem[1]
+                similarity[2] = len(elem[0])
+    
+    return (similarity[0]/max(similarity[2], len(trial1)), similarity[1])
+
 # calculates Pearson correlation 
 # denominator1 is the partial denominator for the first element
 def pearsonCorrelation(el1, e2, denominator1=None):
@@ -92,26 +124,29 @@ def pearsonCorrelation(el1, e2, denominator1=None):
     for condition in el1:
         if (condition in el2):
             # print("Condition in el2")
-            #el2 share some conditions with el1, check the therapies
-            th2 = [x[0] for x in el2[condition]]
+            # el2 share some conditions with el1, check the therapies
+            # th2 = [x[0] for x in el2[condition]]
             for trial in el1[condition]:
                 if(trial[1] != None):
-                    if trial[0] in th2:
-                        t2 = el2[condition][th2.index(trial[0])][1]
-                        if(t2 != None):
-                            # print("Something in common")
-                            if condition in common:
-                                common[condition].append(trial[0])
-                            else:
-                                common[condition] = [trial[0]]
-                            #the same therapy list has been applied between the two
-                            numerator += trial[1]*t2
-        
-        #get the denumerator part for el1
-        if(denominator1 == None):
-            for trial in el1[condition]:
-                if (trial[1] != None):
-                    partial += math.pow(trial[1], 2)
+                    print("FOR CONDITION: "+str(condition)+" AND TRIAL FOR EL1: "+str(trial))
+                    print("COMPARE WITH "+str(el2[condition]))
+                    sim = compareTrials(trial[0], el2[condition])
+                    print(sim)
+                    if sim[0] != 0:
+                        # print("Something in common")
+                        if condition in common:
+                            common[condition].append(trial[0])
+                        else:
+                            common[condition] = [trial[0]]
+                        #the same therapy list has been applied between the two
+                        #TODO: take in consideration the similarity rate
+                        numerator += trial[1]*sim[1]*sim[0]
+            #also the denominator is calculated considering ONLY common conditions
+            #get the denumerator part for el1
+            if(denominator1 == None):
+                for trial in el1[condition]:
+                    if (trial[1] != None):
+                        partial += math.pow(trial[1], 2)
     if (partial == 0):
         partial = 1
     # print("Partial1 for el1: "+str(partial))
@@ -122,9 +157,10 @@ def pearsonCorrelation(el1, e2, denominator1=None):
     #get the denominator part for el2
     partial = 0
     for condition in el2:
-        for trial in el2[condition]:
-            if (trial[1] != None):
-                partial += math.pow(trial[1], 2)
+        if condition in el1:
+            for trial in el2[condition]:
+                if (trial[1] != None):
+                    partial += math.pow(trial[1], 2)
     if(partial == 0):
         partial = 1
     # print("Partial1 for el2: "+str(partial))
@@ -143,7 +179,7 @@ a = normalizePatient(patients[3])
 
 for i in range(0, len(patients)):
     print("PATIENT "+str(i))
-    print("PEARSON:"+str(pearsonCorrelation(patients[3], patients[i])))
+    print("PEARSON:"+str(pearsonCorrelation(a, patients[i])))
     print("\n")
 
 
